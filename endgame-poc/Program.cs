ae86a181ac34cd85b5a236666fcf9be0f29a2aad
15383 <a15383@15383s-MacBook-Pro.local>
2019-04-15 13:02:06 +0100

    updated

---
+++ endgame-poc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+
+namespace endgame_poc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = FluentMockServer.Start();
+
+            server
+                .Given(
+                    Request.Create().WithPath("/some/thing").UsingGet()
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode(200)
+                        .WithHeader("Content-Type", "text/plain")
+                        .WithBody("Hello world!")
+                );
+        }
+    }
+}
