34c101c0ea3c40ecf15fc47d6072f4ef65e571b0
15383 <a15383@15383s-MBP.lan>
2019-04-18 14:18:43 +0100

    new test proj

---
+++ endgame-poc.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+using endgame_poc;
+
+namespace endgame_poc.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            int result = endgame_poc.complexCalc.calculate(1, 2);
+
+            if (result == 3)
+            {
+                Assert.True(true);
+            }
+            else
+            {
+                Assert.False(false);
+            }
+        }
+    }
+}
---
+++ endgame-poc/complexCalc.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace endgame_poc
+{
+    public static class complexCalc
+    {
+        public static int calculate(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
