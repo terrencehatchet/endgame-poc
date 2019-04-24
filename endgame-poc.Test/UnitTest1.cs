61c87dfcc14ddaca786cd7eed9ab83956b180cac
Rhys Morgan <morgan@Rhyss-MacBook-Pro.local>
2019-04-24 16:25:21 +0100

    EG-2 PR-TEST Testing

--- endgame-poc.Test/UnitTest1.cs
+++ endgame-poc.Test/UnitTest1.cs
@@ -11,7 +11,7 @@ namespace endgame_poc.Test
         {
             int result = endgame_poc.complexCalc.calculate(1, 2);
 
-            if (result == 10)
+            if (result == 0)
             {
                 Assert.True(true);
             }
